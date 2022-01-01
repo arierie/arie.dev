b7d7e4c3ffcf28905730087b14ad3548b2d6cd13
arierie <arieridwansyah@gmail.com>
2022-01-01 20:36:21 +0700

    Add preload script to support webflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./controllers/IndexController";
 import About from "./controllers/AboutController";
@@ -6,20 +7,34 @@ import Blog from "./controllers/BlogController";
 import Contact from "./controllers/ContactController";
 import Playlist from "./controllers/PlaylistController";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="playlist" element={<Playlist />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+const WEBFLOW_PAGE_ID = '61610c6cd4489cc64f9a6b2a'
+const WEBFLOW_SITE_ID = '61610c6cd4489c18909a6b26'
+
+class App extends React.Component {
+
+  componentDidMount() {
+    var doc = document.getElementsByTagName("html")[0]
+    doc.setAttribute('data-wf-page', WEBFLOW_PAGE_ID)
+    doc.setAttribute('data-wf-site', WEBFLOW_SITE_ID)
+  };
+
+  render() {
+    return (
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="playlist" element={<Playlist />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    )
+  }
 }
 
 export default App;
---
+++ src/controllers/NavController.js
@@ -0,0 +1,15 @@
+import NavView from '../views/NavView'
+
+const NavController = () => {
+    return (
+        <NavView>
+            <nav-home href="/" />
+            <nav-about href="/about" />
+            <nav-blog href="/blog" />
+            <nav-contact href="/contact" />
+            <nav-playlist href="/playlist" />
+        </NavView>
+    )
+}
+
+export default NavController
